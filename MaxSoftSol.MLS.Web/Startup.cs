30974c72a17c344af93c37b2c579375196d7a541
Maxim Gunter <gunter.maxim1@gmail.com>
2017-03-11 14:46:51 -0500

    Initial Commit

---
+++ MaxSoftSol.MLS.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MaxSoftSol.MLS.Web.Startup))]
+namespace MaxSoftSol.MLS.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
