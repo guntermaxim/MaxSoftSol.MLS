51f0c07dad8239c6aa6a4ea4c2660b46615d2a28
Maxim Gunter <gunter.maxim@gmail.com>
2017-03-15 17:28:57 -0400

    Added Resources

---
+++ MaxSoftSol.MLS.Resources/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxSoftSol.MLS.Resources
+{
+    public class Class1
+    {
+    }
+}
